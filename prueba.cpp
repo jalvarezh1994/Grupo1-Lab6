cac6b6a44f242e0d5d0615d1f89211a1b62d12e5
ChrizzFeller <chrizzhenriquez@unitec.edu>
2017-05-26 19:49:54 -0600

    Trabajando ~~~

--- prueba.cpp
+++ prueba.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <typeinfo>
+#include <fstream>
 #include "Usuario.h"
 #include "Cliente.h"
 #include "Personal.h"
@@ -20,6 +21,7 @@ double aumentarS(double);
 double decrementarS(double);
 vector<int> menorS(vector<Usuario*>);
 vector<int> mayorS(vector<Usuario*>);
+void guardarArchivo(vector<Usuario*>);
 
 int main() {
 	vector<Usuario*> usuarios;
@@ -769,4 +771,107 @@ vector<int> mayorS(vector<Usuario*> usuarios) {
 	}
 
 	return mayores;
+}
+
+void guardarArchivo(vector<Usuario*> Usuarios){
+	//Clientes
+	string Clientes="";
+	string Administradores="";
+	string Chefs="";
+	string Lavaplatoss="";
+	string Meseros="";
+	for (int i = 0; i < Usuarios.size(); ++i)
+	{
+		if(dynamic_cast<Cliente*>(Usuarios[i]))
+		{
+			Cliente* temp=dynamic_cast<Cliente*>(Usuarios[i]);
+			Clientes+=temp->getUsername()+" ";
+			Clientes+=temp->getNombre()+" ";
+			Clientes+=temp->getPassword()+" ";
+			Clientes+=to_string(temp->getEdad())+" ";
+			Clientes+=temp->getId()+" ";
+			Clientes+=temp->getTelefono()+" ";
+			Clientes+=temp->getDireccion()+" ";
+			Clientes+=to_string(temp->getEstrellas())+" ";
+			Clientes+="\n";
+		}
+		if(dynamic_cast<Administrador*>(Usuarios[i]))
+		{
+			Administrador* temp=dynamic_cast<Administrador*>(Usuarios[i]);
+			Administradores+=temp->getUsername()+" ";
+			Administradores+=temp->getNombre()+" ";
+			Administradores+=temp->getPassword()+" ";
+			Administradores+=to_string(temp->getEdad())+" ";
+			Administradores+=temp->getId()+" ";
+			Administradores+=temp->getTelefono()+" ";
+			Administradores+=to_string(temp->getAnioContratado())+" ";
+			Administradores+=to_string(temp->getSueldo())+" ";
+			Administradores+=to_string(temp->getContratados())+" ";
+			Administradores+=to_string(temp->getDespedidos())+" ";
+			Administradores+="\n";
+		}
+		if(dynamic_cast<Chef*>(Usuarios[i]))
+		{
+			Chef* temp=dynamic_cast<Chef*>(Usuarios[i]);
+			Chefs+=temp->getUsername()+" ";
+			Chefs+=temp->getNombre()+" ";
+			Chefs+=temp->getPassword()+" ";
+			Chefs+=to_string(temp->getEdad())+" ";
+			Chefs+=temp->getId()+" ";
+			Chefs+=temp->getTelefono()+" ";
+			Chefs+=to_string(temp->getAnioContratado())+" ";
+			Chefs+=to_string(temp->getSueldo())+" ";
+			Chefs+=temp->getPlatilloFavorito()+" ";
+			Chefs+=to_string(temp->getRating())+" ";
+			Chefs+="\n";
+		}
+		if(dynamic_cast<Lavaplatos*>(Usuarios[i]))
+		{
+			Lavaplatos* temp=dynamic_cast<Lavaplatos*>(Usuarios[i]);
+			Lavaplatoss+=temp->getUsername()+" ";
+			Lavaplatoss+=temp->getNombre()+" ";
+			Lavaplatoss+=temp->getPassword()+" ";
+			Lavaplatoss+=to_string(temp->getEdad())+" ";
+			Lavaplatoss+=temp->getId()+" ";
+			Lavaplatoss+=temp->getTelefono()+" ";
+			Lavaplatoss+=to_string(temp->getAnioContratado())+" ";
+			Lavaplatoss+=to_string(temp->getSueldo())+" ";
+			Lavaplatoss+=to_string(temp->getMotivacion())+" ";
+			Lavaplatoss+="\n";
+		}
+		if(dynamic_cast<Mesero*>(Usuarios[i]))
+		{
+			Mesero* temp=dynamic_cast<Mesero*>(Usuarios[i]);
+			Meseros+=temp->getUsername()+" ";
+			Meseros+=temp->getNombre()+" ";
+			Meseros+=temp->getPassword()+" ";
+			Meseros+=to_string(temp->getEdad())+" ";
+			Meseros+=temp->getId()+" ";
+			Meseros+=temp->getTelefono()+" ";
+			Meseros+=to_string(temp->getAnioContratado())+" ";
+			Meseros+=to_string(temp->getSueldo())+" ";
+			Meseros+="\n";
+		}
+	}
+	cout<<Clientes;
+	ofstream archivo;
+	string nombreArchivo;
+	archivo.open("Clientes.txt",ios::out);
+	archivo<<Clientes;
+	archivo.close();
+	archivo.open("Administradores.txt",ios::out);
+	archivo<<Administradores;
+	archivo.close();
+	archivo.open("Chefs.txt",ios::out);
+	archivo<<Chefs;
+	archivo.close();
+	archivo.open("Administradores.txt",ios::out);
+	archivo<<Administradores;
+	archivo.close();
+	archivo.open("Lavaplatos.txt",ios::out);
+	archivo<<Lavaplatoss;
+	archivo.close();
+	archivo.open("Meseros.txt",ios::out);
+	archivo<<Meseros;
+	archivo.close();
 }
